a2cbecad0be0b0efc7b460b22807791adb1460b8
Saddam Hafiz <saddam.hafiz.dhaka@gmail.com>
2023-04-01 15:58:29 +0600

    complete full responsive newsletter section

---
+++ app.js
@@ -0,0 +1,11 @@
+// Show Cards
+const cards = document.querySelector(".card-wrap");
+const saddam = document.getElementById("see-more");
+console.log(cards);
+console.log(saddam);
+
+saddam.addEventListener("click", function() {
+    console.log("hellow")
+})
+
+
