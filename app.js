e5a2d229fd656d43e9b12314334b24e77602674c
Saddam Hafiz <saddam.hafiz.dhaka@gmail.com>
2023-04-02 20:18:40 +0600

    setup fixed navber

--- app.js
+++ app.js
@@ -7,3 +7,24 @@ toggleBtn.addEventListener("click", function() {
     toggleBtn.classList.toggle("rotate");
 })
 
+/** Fixed Nav **/ 
+const navber = document.querySelector(".navber");
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", function () {
+    const scrollHeight = window.pageYOffset
+    const navHeight = navber.getBoundingClientRect().height;
+
+    if(navHeight < scrollHeight) {
+        navber.classList.add("fixed-nav");
+    }else{
+        navber.classList.remove("fixed-nav");
+    }
+
+    if(scrollHeight > 50) {
+        topLink.classList.add("show-link");
+    }else{
+        topLink.classList.remove("show-link");
+    }
+})
+
